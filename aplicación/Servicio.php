0a6f5e8d295eae9d1a86a05c39774cf6ca5b3929
Vero-Jani <124016816+Vero-Jani@users.noreply.github.com>
2023-01-30 22:56:34 -0500

    Update and rename app/Servicio.php to aplicación/Servicio.php

--- aplicación/Servicio.php
+++ aplicación/Servicio.php
@@ -11,10 +11,7 @@ class Servicio{
     public function __construct(){
         if(!$this->dbConnect){ 
             $conn = new mysqli($this->host, $this->user, $this->password, $this->database);
-            if($conn->connect_error){
-                die("Error failed to connect to MySQL: " . $conn->connect_error);
-            }else{
-                $this->dbConnect = $conn;
+           is->dbConnect = $conn;
             }
         }
     }
